d079be160f13a8864188867d6ecdfd786d317387
Tatum Sterling <tatum.sterling29@gmail.com>
2023-11-30 11:10:08 -0600

    added Child model

---
+++ models/Child.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Child extends Model {}
+
+Child.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        },
+      },
+      status: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'child',
+    }
+  );
+  
+  module.exports = Child;

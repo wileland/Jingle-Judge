c848d927ee3f533ef4b57dbfd75375e7e5bfcc06
wileland <wileland7@gmail.com>
2023-12-04 09:35:23 -0600

    Add base routes userRoutes and added to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,14 @@
-//todo fill with code
+const router = require('express').Router();
+
+// Import all other routers
+const apiRoutes = require('./api');
+const viewRoutes = require('./viewRoutes'); // If you have routes for server-rendered views
+const userRoutes = require('./api/userRoutes'); // Assuming you have user routes inside the api folder
+
+// Establish the base routes
+router.use('/api', apiRoutes);
+router.use('/', viewRoutes);
+router.use('/api/users', userRoutes);
+
+// Export the base router
+module.exports = router;

a72c17f44cebee6062ce7406dccbf85a385dfca2
wileland <wileland7@gmail.com>
2023-12-04 23:02:36 -0600

    Fix file paths and update dependencies

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,25 +1,34 @@
-let days = document.querySelector('.days .number'),
-    hours = document.querySelector('.hours .number'),
-    minutes = document.querySelector('.minutes .number'),
-    seconds = document.querySelector('.seconds .number');
+// countdown.js
+let days = document.querySelector(".days .number"),
+  hours = document.querySelector(".hours .number"),
+  minutes = document.querySelector(".minutes .number"),
+  seconds = document.querySelector(".seconds .number");
 
-    countDownDate = new Date("Dec 24, 2023 23:59:59").getTime();
+let countDownDate = new Date("Dec 24, 2023 23:59:59").getTime();
 
-    let counter = setInterval(() => {
-       let dateNow = new Date().getTime();
-       let dateDiff = countDownDate - dateNow;
-       
-       let day = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-       let hour = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-       let minute = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-       let second = Math.floor((dateDiff % (1000 * 60)) / 1000);
+let updateCountdown = () => {
+  let dateNow = new Date().getTime();
+  let dateDiff = countDownDate - dateNow;
 
-       days.innerHTML = day < 10 ? `0${day}` : day;
-       hours.innerHTML = hour    < 10 ? `0${ hour }` : hour;
-       minutes.innerHTML = minute < 10 ? `0${minute}` : minute;
-       seconds.innerHTML = second < 10 ? `0${second}` : second;
+  let day = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hour = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minute = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let second = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
-       if (dateDiff == 0) {
-        clearInterval(counter);
-       }
-    }, 1000);
+  days.innerHTML = day < 10 ? `0${day}` : day;
+  hours.innerHTML = hour < 10 ? `0${hour}` : hour;
+  minutes.innerHTML = minute < 10 ? `0${minute}` : minute;
+  seconds.innerHTML = second < 10 ? `0${second}` : second;
+
+  if (dateDiff <= 0) {
+    clearInterval(counter);
+    days.innerHTML = "00";
+    hours.innerHTML = "00";
+    minutes.innerHTML = "00";
+    seconds.innerHTML = "00";
+  }
+};
+
+// Update the countdown immediately upon loading the page
+updateCountdown();
+let counter = setInterval(updateCountdown, 1000);
